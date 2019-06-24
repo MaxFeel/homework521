fa6e46a55a9029bb9233d0320b3e32c0e79a4ae5
maximtosh <mr.maks.filippov.94@mail.ru>
2019-06-24 14:18:37 +0300

    update

--- app/src/main/java/com/example/homework521/MainActivity.java
+++ app/src/main/java/com/example/homework521/MainActivity.java
@@ -8,8 +8,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
     private EditText mPasswordEditText;
     Toast ex;
     Button registration;
+    Button login;
+    String fileNameLogin;
+    String fileNamePass;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +34,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mLoginEditText = findViewById(R.id.edit_login);
         mPasswordEditText = findViewById(R.id.edit_password);
+
+        fileNameLogin = "logFile";
+        fileNamePass = "passFile";
+
+
         registration = findViewById(R.id.buttonRegistration);
         registration.setOnClickListener(regListner);
+        login = findViewById(R.id.buttonLogin);
+        login.setOnClickListener(loginListner);
 
 
     }
@@ -34,8 +50,6 @@ public class MainActivity extends AppCompatActivity {
     private final View.OnClickListener regListner = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            String fileNameLogin = "logFile";
-            String fileNamePass = "passFile";
             FileOutputStream outputStreamLogin;
             FileOutputStream outputStreamPass;
             try {
@@ -43,13 +57,15 @@ public class MainActivity extends AppCompatActivity {
                     outputStreamLogin = openFileOutput(fileNameLogin, Context.MODE_PRIVATE);
                     OutputStreamWriter outputStreamWriterLogin = new OutputStreamWriter(outputStreamLogin);
                     BufferedWriter bwLogin = new BufferedWriter(outputStreamWriterLogin);
-                    bwLogin.write(mLoginEditText.toString());
+                    String textLogin = mLoginEditText.getText().toString();
+                    bwLogin.write(textLogin);
                     bwLogin.close();
 
                     outputStreamPass = openFileOutput(fileNamePass, Context.MODE_PRIVATE);
                     OutputStreamWriter streamWriterPass = new OutputStreamWriter(outputStreamPass);
                     BufferedWriter bwPass = new BufferedWriter(streamWriterPass);
-                    bwPass.write(mPasswordEditText.toString());
+                    String textPass = mPasswordEditText.getText().toString();
+                    bwPass.write(textPass);
                     bwPass.close();
                     //ex.makeText(getApplicationContext(), "Успешная регистрация", Toast.LENGTH_LONG);
                     Toast.makeText(MainActivity.this, "Успешная регистрация", Toast.LENGTH_LONG).show();
@@ -62,6 +78,44 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+
+
+    private final View.OnClickListener loginListner = new View.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+            try {
+                FileInputStream fileInputLogin = openFileInput(fileNameLogin);
+                InputStreamReader inputStreamReaderLogin = new InputStreamReader(fileInputLogin);
+                BufferedReader bfrLogin = new BufferedReader(inputStreamReaderLogin);
+                String lineLogin = bfrLogin.readLine();
+
+
+                FileInputStream fileInputPass = openFileInput(fileNamePass);
+                InputStreamReader inputStreamReaderPass = new InputStreamReader(fileInputPass);
+                BufferedReader bfrPass = new BufferedReader(inputStreamReaderPass);
+                String linePass = bfrPass.readLine();
+
+
+                if(mLoginEditText.getText().toString().equals(lineLogin)
+                        && mPasswordEditText.getText().toString().equals(linePass)){
+                    Toast.makeText(MainActivity.this, "Успешный вход", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Неверный логин или пароль", Toast.LENGTH_LONG).show();
+                }
+
+
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    };
+
     public void clickRegistration(View view) {
 
     }
